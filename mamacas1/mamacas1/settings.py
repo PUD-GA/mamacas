7fbf3973800c4e1f8b3fa8e834184695270e3a8a
etpistezmots <beligne.max@gmail.com>
2024-08-23 11:33:55 +0200

    settings + urls + views first work2

--- mamacas1/mamacas1/settings.py
+++ mamacas1/mamacas1/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mamacas',
         'USER': 'mamacas',
-        'PASSWORD': 'mamacas',
+        'PASSWORD': 'test',
         'HOST': 'localhost',
         'PORT': '',
     }}
