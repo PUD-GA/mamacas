f55708e491f37b280fa1f68bb28265fa3691da23
etpistezmots <beligne.max@gmail.com>
2024-08-23 15:51:42 +0200

    change debug2

--- mamacas1/mamacas1/settings.py
+++ mamacas1/mamacas1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-amo^weuk@3dnirn8^$ppbe*)2jjl$cvq%dr7_f7gx-a(l8hdrk'
+SECRET_KEY = config("MY_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
