a97b60ab40be402a9df570d9987e4ff715718f41
etpistezmots <beligne.max@gmail.com>
2024-09-03 09:21:19 +0200

    add test cors pour tadddam

--- mamacas1/mamacas1/settings.py
+++ mamacas1/mamacas1/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'mama_cas'
 ]
 
@@ -47,12 +48,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ['https://tadddam.univ-grenoble-alpes.fr/public/']
 
 ROOT_URLCONF = 'mamacas1.urls'
 
